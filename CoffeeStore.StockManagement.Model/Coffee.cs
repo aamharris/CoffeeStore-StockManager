ad1a248ede29a32f9baa13707301ee7ebce7c97a
Aaron Harris <Aaron Harris@DESKTOP-TGG1B63>
2016-04-07 21:19:38 -0500

    Add project files.

---
+++ CoffeeStore.StockManagement.Model/Coffee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeStore.StockManagement.Model
+{
+    public class Coffee
+    {
+        public int CoffeeId { get; set; }
+        public string CoffeeName { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
+        public Country OriginCountry { get; set; }
+        public bool InStock { get; set; }
+        public int InventoryAmount { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int ImageId { get; set; }
+    }
+}
