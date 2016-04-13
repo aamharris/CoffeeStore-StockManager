2b2f7360a2e987e92c93952b8342de53336b2fcb
Aaron Harris <aamharris@gmail.com>
2016-04-13 08:15:59 -0500

    Add view model locator. Updated app with mvvm.

---
+++ CoffeeStore.StockManagement.App/ViewModelLocator.cs
@@ -0,0 +1,31 @@
+﻿using CoffeeStore.StockManagement.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeStore.StockManagement.App
+{
+    public class ViewModelLocator
+    {
+        private static CoffeeOverviewViewModel coffeeOverviewViewModel = new CoffeeOverviewViewModel();
+        private static CoffeeDetailViewModel coffeeDetailViewModel = new CoffeeDetailViewModel(); 
+
+        public static CoffeeDetailViewModel CoffeeDetailViewModel
+        {
+            get
+            {
+                return coffeeDetailViewModel;
+            }
+        }
+
+        public static CoffeeOverviewViewModel CoffeeOverviewViewModel
+        {
+            get
+            {
+                return coffeeOverviewViewModel;
+            }
+        }
+    }
+}
