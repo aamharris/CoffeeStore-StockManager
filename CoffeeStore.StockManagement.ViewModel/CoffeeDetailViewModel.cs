8dde3598160aebf779e5758fe601beb01744be4b
Aaron Harris <aamharris@gmail.com>
2016-04-13 08:12:29 -0500

    Add View Model Project

---
+++ CoffeeStore.StockManagement.ViewModel/CoffeeDetailViewModel.cs
@@ -0,0 +1,54 @@
+﻿using CoffeeStore.StockManagement.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeStore.StockManagement.ViewModel
+{
+    public class CoffeeDetailViewModel : INotifyPropertyChanged
+    {
+        private Coffee selectedCoffee;
+
+        public Coffee SelectedCoffee
+        {
+            get
+            {
+                return selectedCoffee;
+            }
+            set
+            {
+                selectedCoffee = value;
+                RaisePropertyChanged(GetPropertyName(() => this.selectedCoffee));
+            }
+
+        }
+
+        public CoffeeDetailViewModel()
+        {
+          
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string GetPropertyName<T>(Expression<Func<T>> propertyLambda)
+        {
+            var me = propertyLambda.Body as MemberExpression;
+
+            if (me == null)
+            {
+                throw new ArgumentException("You must pass a lambda of the form: '() => Class.Property' or '() => object.Property'");
+            }
+
+            return me.Member.Name;
+        }
+    }
+}
---
+++ CoffeeStore.StockManagement.ViewModel/CoffeeOverviewViewModel.cs
@@ -0,0 +1,84 @@
+﻿using CoffeeStore.StockManagement.Extensions;
+using CoffeeStore.StockManagement.Model;
+using CoffeeStore.StockManagement.Services;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Windows.Input;
+
+namespace CoffeeStore.StockManagement.ViewModel
+{
+    public class CoffeeOverviewViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Coffee> coffees;
+        private CoffeeDataService coffeeDataService;
+        private Coffee selectedCoffee;
+
+        public ICommand EditCommand { get; set; }
+
+        public ObservableCollection<Coffee> Coffees
+        {
+            get
+            {
+                return coffees;            
+            }
+            set
+            {
+                coffees = value;
+                RaisePropertyChanged(GetPropertyName(() => this.coffees));
+            }
+
+        }
+
+        public Coffee SelectedCoffee
+        {
+            get
+            {
+                return selectedCoffee;
+            }
+            set
+            {
+                selectedCoffee = value;
+                RaisePropertyChanged(GetPropertyName(() => this.selectedCoffee));
+            }
+
+        }
+
+        public CoffeeOverviewViewModel()
+        {
+            coffeeDataService = new CoffeeDataService();
+            LoadData();
+            LoadCommands();
+        }
+
+        private void LoadCommands()
+        {
+            //EditCommand = new CustomCommand()
+        }
+
+        private void LoadData()
+        {
+            Coffees = coffeeDataService.GetAllCoffees().ToObservableCollection(); 
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string GetPropertyName<T>(Expression<Func<T>> propertyLambda)
+        {
+            var me = propertyLambda.Body as MemberExpression;
+
+            if (me == null)
+            {
+                throw new ArgumentException("You must pass a lambda of the form: '() => Class.Property' or '() => object.Property'");
+            }
+
+            return me.Member.Name;
+        }
+    }
+}
